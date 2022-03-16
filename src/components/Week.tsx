4d7d08fc32b0d4c2a540ccba82c64c7bb78e4b4b
Yash <yashvadhvani@gmail.com>
2022-03-16 23:35:15 +0530

    fix: 🐛 No classes available for the external users

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -5,7 +5,7 @@ interface IWeekProps {
   weeks: any[];
 }
 const Week = ({ weeks }: IWeekProps) => (
-  <div className="grid grid-cols-7 py-2 mt-0.5 border-b border-black border-opacity-10 dark:text-litepie-secondary-200 dark:border-litepie-secondary-700 dark:border-opacity-100">
+  <div className="grid grid-cols-7 py-2 mt-0.5 border-b border-black border-opacity-10 dark:text-litepie-secondary-200 dark:border-litepie-secondary-700 dark:border-opacity-100 litepie-week">
     {weeks.map((day: any, index: number) => (
       <div key={index}>
         <span> {day}</span>
